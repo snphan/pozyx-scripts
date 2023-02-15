7dcec1b50c513d132bfaa2119b98db21b2a5542c
Steven Phan <snphan@ualberta.ca>
2023-02-15 10:24:10 -0700

    add z visualization

--- plot_data.py
+++ plot_data.py
@@ -8,6 +8,9 @@ import sys
 from config import constants
 from pathlib import Path
 
+"""
+Plot the data in realtime
+"""
 
 data_path = Path(__file__).resolve().parents[0].joinpath("data")
 
@@ -33,24 +36,34 @@ mydata = deque([1,2,3,4,5])
 img = Image.open(bg_path).convert("L")
 img = np.asarray(img)
 
-remote_id = constants.REMOTE_IDS  # remote device network ID
+remote_id = ["0x%0.4x" % id for id in constants.REMOTE_IDS]  # remote device network ID
 buffer = {}
 
-for id in remote_id:
-    tag_id =  "0x%0.4x" % id
+for tag_id in remote_id:
     buffer[tag_id] = {}
     buffer[tag_id]["x"] = []
     buffer[tag_id]["y"] = []
+    buffer[tag_id]["z"] = []
 
 # Create figure for plotting
 fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+(ax, axzpos) = fig.subplots(1,2, width_ratios=[3, 1])
+axzpos.set_ylim(-100, 5000)
+axzpos.set_xlim(-1, 4)
 
 # Create a blank line. We will update the line in animate
 lines = {}
 for k in buffer:
+    # Plot xy
+    lines[k] = {}
     line, = ax.plot(buffer[k]["x"], buffer[k]["y"], label=k)
-    lines[k] = line
+    lines[k]["xy"] = line
+
+    # Plot z
+    line, = axzpos.plot([remote_id.index(k)]*(len(buffer[k]["z"])), buffer[k]["z"], 'o-', label=k)
+    lines[k]["z"] = line
+
+
 
 ax.imshow(img, extent=[0,img.shape[1]*bg_multiplier,0,img.shape[0]*bg_multiplier], cmap='gray', vmin=0, vmax=255)
 # Add labels
@@ -65,24 +78,31 @@ def animate(i, buffer):
     for k in buffer:
         buffer[k]["x"] = []
         buffer[k]["y"] = []
+        buffer[k]["z"] = []
 
     with open(data_file, 'r') as f:
         all_data = f.readlines()
         data = all_data[-20:]
         for one_data in data:
             splitted = one_data.split(",")
-            k = splitted[-1].replace("\n", "") 
+            key = splitted[-1].replace("\n", "") 
             x = float(splitted[1])
             y = float(splitted[2])
-            buffer[k]["x"].append(x)
-            buffer[k]["y"].append(y)
+            z = float(splitted[3])
+            buffer[key]["x"].append(x)
+            buffer[key]["y"].append(y)
+            buffer[key]["z"].append(z)
 
+    # Limit z to 3 points
+    buffer[key]["z"] = buffer[key]["z"][-3:]
     # Update line with new Y values
     for k in lines:
-        lines[k].set_xdata(buffer[k]["x"])
-        lines[k].set_ydata(buffer[k]["y"])
+        lines[k]["xy"].set_xdata(buffer[k]["x"])
+        lines[k]["xy"].set_ydata(buffer[k]["y"])
+        lines[k]["z"].set_ydata(buffer[k]["z"])
+        lines[k]["z"].set_xdata([remote_id.index(k)]*len(buffer[k]["z"]))
 
-    return lines.values()
+    return [lines[tagid][data_type] for tagid in lines for data_type in lines[tagid]]
 
 # Set up plot to call animate() function periodically
 ani = animation.FuncAnimation(fig,
